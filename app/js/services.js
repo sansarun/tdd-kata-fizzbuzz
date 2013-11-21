81a1f457e01e7c4073c403095a537b93aed41a85
sansarun <sansarun@gmail.com>
2013-11-22 00:47:10 +0700

    add more test case to fizzbuzzServiceSpec

--- app/js/services.js
+++ app/js/services.js
@@ -3,7 +3,23 @@ var module = angular.module('fizzbuzz.services', []);
 module.service('fizzbuzzService', function() {
 	return {
 		answer: function(number) {
-			return "?";
+
+			if(number <= 0) {
+				throw new Error("input out of range");
+			}
+
+			if(number % 3 == 0 && number % 5 != 0) {
+				return 'fizz';
+			}
+			else if(number % 5 == 0 && number % 3 != 0) {
+				return 'buzz';
+			}
+			else if(number % 3 == 0 && number % 5 == 0) {
+				return 'fizzbuzz';
+			}
+			else if(number % 3 != 0 && number % 5 != 0) {
+				return number;
+			}
 		}
   	}
 });
